7e7329500aefd0bdc8b6f28f63acda3314574db9
Carla <100550492@alumnos.uc3m.es>
2026-02-17 21:48:48 +0100

    cambios lab 1

---
+++ Core/Src/main.c
@@ -0,0 +1,443 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+extern UART_HandleTypeDef huart2;
+
+int _write(int file, char *ptr, int len) {
+	int i=0;
+	for(i=0; i<len; i++)
+	HAL_UART_Transmit(&huart2,(uint8_t *)(ptr++),1,1000);
+	return len;
+}
+void waiting(unsigned int delay) {
+	unsigned int i;
+	for (i=0; i<delay; i++);
+}
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+unsigned char state = 0;
+unsigned char medicion_init = 0;
+unsigned char medicion_end = 1;
+uint32_t value_adc = 0;
+int angle = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void EXTI15_10_IRQHandler(void){
+	if(EXTI->PR & (1 << 13)){
+		EXTI->PR = (0x01 << 13);
+		state = state + 1;
+		if (state >1) state = 0;
+		if (medicion_end == 0) printf("El cambio se realizara al terminar la medicion \r\n");
+	}
+}
+
+void EXTI4_IRQHandler(void){
+	// no need to verify EXTI PR as it only triggers this function with EXTI4
+	EXTI->PR = (0x01 << 4);
+	medicion_init = 1;
+	medicion_end = 0;
+
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+	unsigned char prev_state = 0;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  /*INPUTS: */
+  /*PB4 configuration EXTI mode, moder (00)*/
+  GPIOB ->MODER &= ~(1 << (4*2 + 1));
+  GPIOB ->MODER &= ~(1 << (4*2));
+  GPIOB ->PUPDR &= ~(3 << 8); // cleaning any previous configuration
+  GPIOB ->PUPDR |= (2 << 8); // Setting button on pull down (10)
+
+  SYSCFG ->EXTICR[1] &= ~(0xF << 0);	//Cleaning any new previous at SYSCFG
+  SYSCFG->EXTICR[1] |= (0x01 << 0);		// Enabling EXTI4 for PB4 with 0001
+  EXTI ->FTSR &= ~(0x01 << 4);			// Disabling falling edge
+  EXTI ->RTSR |= (0x01 << 4);			// Enabling rising edge
+  EXTI ->IMR |= (0x01 << 4); 			// Enable EXTI 4
+  NVIC ->ISER[0] |= (1 << 10); 	// Enable EXTI4 NVIC
+
+  /*PC13 configuration EXTI mode, moder(00)*/
+  GPIOC ->MODER &= ~(1 << (13*2 + 1));
+  GPIOC ->MODER &= ~(1 << (13*2));
+
+  SYSCFG ->EXTICR[3] &= ~(0xF << 4);	//Cleaning any new previous at SYSCFG
+  SYSCFG->EXTICR[3] |= (0x02 << 4);		// Enabling EXTI13 for PC13 with 0010
+  EXTI ->FTSR |= (0x01 << 13);			// Enable falling edge
+  EXTI ->RTSR &= ~(0x01 << 13);			// Disable rising edge
+  EXTI ->IMR |= (0x01 << 13); 			// Enable EXTI 13
+  NVIC ->ISER[1] |= (1 << (40-32)); 	// Enable EXTI15_10 NVIC
+
+  /*OUTPUTS: */
+  /*PA5 configuration in output mode, moder(01)  */
+  GPIOA ->MODER &= ~(1 << (5*2 + 1));
+  GPIOA ->MODER |= (1 << (5*2));
+
+
+  /*PB5 configuration of the external led, moder(01)*/
+  GPIOB ->MODER &= ~(1 << (5*2 + 1));
+  GPIOB ->MODER |= (1 << (5*2));
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  if ((state != prev_state) && (medicion_end == 1)){
+		  prev_state = state;
+		  switch(state){
+		  case 0: //Manual Mode led off
+			  printf("Modo manual \r\n");
+			  GPIOA->BSRR = (1<<5)<<16;
+			  break;
+
+		  default: //Automatic Mode led onn
+			  printf("Modo automatico \r\n");
+			  GPIOA->BSRR = (1<<5);
+		  }
+	  }
+	  if (state == 0){
+		  /*Reading ADC*/
+		  if(medicion_init == 1){
+			  medicion_init = 0;
+			  //reads the potenciometer (ADC)
+			  //Start Conversion
+			  ADC1->CR2 |= 0x40000000;
+			  //Wait for the conversion to finish
+			  while((ADC1->SR & 0x0002)==0);
+			  //read the result (Data Register)
+			  value_adc = ADC1->DR;
+			  //Compute the angle (0 to 180)
+			  angle = (value_adc * 180)/4095;
+			  //Print the value
+			  printf("Modo Manual -> Angulo del servomotor: %d \r\n", angle);
+			  //Pulse sent to the servo
+			  GPIOB->BSRR = (1<<5);
+			  waiting(4000000); //0.5s this must be changed after by a timer
+			  GPIOB->BSRR = (1<<5)<<16;
+			  medicion_end = 1;
+		  }
+	  }else{
+		  if(medicion_init == 1){
+			  medicion_init = 0;
+			  for(int i = 0; i<5; i++){
+				  angle = -90 + i*45;
+				  printf("Modo automatico -> Angulo del servomotor: %d\r\n", angle);
+				  for(int j = 0; j<2;j++){
+				  	  GPIOB->BSRR = (1<<5);
+				  	  waiting(4000000);//0.5s this must be changed after by a timer
+				  	  GPIOB->BSRR = (1<<5)<<16;
+				  	  waiting(4000000);//0.5s this must be changed after by a timer
+				  }
+			  }
+			  printf("Volviendo a posicion inicial -> Angulo del servomotor: 0 \r\n");
+		  }
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc.Init.EOCSelection = ADC_EOC_SEQ_CONV;
+  hadc.Init.LowPowerAutoWait = ADC_AUTOWAIT_DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = ADC_AUTOPOWEROFF_DISABLE;
+  hadc.Init.ChannelsBank = ADC_CHANNELS_BANK_A;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.NbrOfConversion = 1;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_4CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+
+  /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
